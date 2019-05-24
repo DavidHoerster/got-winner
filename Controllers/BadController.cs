8f25854d2e60be52197d31f9a9663e899de38f97
David Hoerster <dahoer@microsoft.com>
2019-05-24 11:18:09 -0400

    #54 - GoT Bug for 500 error - Issue with EXCEPTION System.InvalidOperationException

--- Controllers/BadController.cs
+++ Controllers/BadController.cs
@@ -12,17 +12,17 @@ namespace got_winner_voting.Controllers
         [HttpGet]
         public async Task<ActionResult> Get()
         {
-            // make it slow, too, and then fail
-            await Task.Delay(1500);
+            // // make it slow, too, and then fail
+            // await Task.Delay(1500);
 
-            var x = 1;
-            if (x == 1)
-            {
-                throw new InvalidOperationException("You tried to do something bad");
-            }
+            // var x = 1;
+            // if (x == 1)
+            // {
+            //     throw new InvalidOperationException("You tried to do something bad");
+            // }
 
-            return BadRequest();
-            // return Ok("all good now!");
+            // return BadRequest();
+            return Ok("all good now!");
         }
 
         [HttpGet("slow")]
